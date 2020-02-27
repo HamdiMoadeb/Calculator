ffaa258f047719d7f914d414c122a59e1275f18a
Hamdi Moadeb <hamdimoadeb@gmail.com>
2020-02-27 17:42:32 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by MacBookPro on 3/7/19.
+//  Copyright © 2019 Saphir Consult. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+}
+
